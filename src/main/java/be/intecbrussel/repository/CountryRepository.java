c699f628f4e5ad2e8ca7972e26a40f46ce632e09
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2023-02-27 14:55:35 +0100

    Update2

--- src/main/java/be/intecbrussel/repository/CountryRepository.java
+++ src/main/java/be/intecbrussel/repository/CountryRepository.java
@@ -7,21 +7,36 @@ import java.util.Optional;
 
 public class CountryRepository {
     //Connect to the DB
+
+    //If you modify the DB
+    //1. Begin transaction
+    //2. Do DB stuff
+    //3. Commit transaction
+    //4. Return object if needed
+
     public void createCountry(EntityManager em, Country country) {
         em.getTransaction().begin();
-
         em.persist(country);
-
         em.getTransaction().commit();
        // em.getTransaction().rollback(); other method
     }
+
+    //If you only read from DB
+    //1. Read from DB
+    //2. Return object
+
     public Optional<Country> readCountry(EntityManager em, String name) {
         Country country = em.find(Country.class, name);
-        return Optional.ofNullable(country);
+        return Optional.ofNullable(country); //
     }
     public void deleteCountry(EntityManager em, Country country) {
         em.getTransaction().begin();
         em.remove(country);
+        em.getTransaction().commit(); //do it
+    }
+    public void updateCountry(EntityManager em, Country country) {
+        em.getTransaction().begin();
+        em.merge(country);
         em.getTransaction().commit();
     }
 }
